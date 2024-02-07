f8f8e19e06489e47870ffb99a56cd8f99e04f01b
surajkachate <surajkachate021@gmail.com>
2024-02-07 19:18:48 +0530

    restaurent menu commit

---
+++ restaurent-menu/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Restaurent from './component/Basics/Restaurent'
+
+const App = () => {
+  return (
+    <div>
+      <Restaurent/>
+    </div>
+  )
+}
+
+export default App
+
